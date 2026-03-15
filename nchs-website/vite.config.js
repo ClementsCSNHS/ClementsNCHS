dc6651690eb1dc4935c48e6aa7e9a697fe4e77d4
Claire <claire.weng.2009@gmail.com>
2026-03-15 00:41:29 -0500

    fixes

--- nchs-website/vite.config.js
+++ nchs-website/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import markdown from 'vite-plugin-markdown';
+import { plugin as markdown, Mode } from 'vite-plugin-markdown';
 
 export default defineConfig({
   plugins: [
     react(),
-    markdown({ mode: 'html' }),
+    markdown({ mode: [Mode.HTML, Mode.FRONTMATTER] }),
   ],
 });
